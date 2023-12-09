31d25baa4655d606da99af42224ca350e7fe0bbf
cpzt <chenpan9012@gmail.com>
2023-12-09 16:03:20 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tools',
+    version='0.0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={
+        'jenkins_tool': [
+            "python_jenkins"
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
